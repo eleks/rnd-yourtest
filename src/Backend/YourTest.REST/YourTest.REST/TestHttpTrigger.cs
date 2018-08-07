20cdc17c93b99400daaccc1618b41a57e79ddce7
Roman Rudyak <roma.rudyak@gmail.com>
2018-08-07 10:02:56 +0300

    Add design of rest api for test
    GET /api/test
    GET /api/test/{id}/summery
    PUT /api/test/{id}/status
    GET /api/test/{id}
    POST /api/test/{id}

---
+++ src/Backend/YourTest.REST/YourTest.REST/TestHttpTrigger.cs
@@ -0,0 +1,79 @@
+
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json;
+using System;
+
+namespace YourTest.REST
+{
+    public static class TestHttpTrigger
+    {
+        [FunctionName(nameof(GetAllTests))]
+        public static IActionResult GetAllTests(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "test")]
+            HttpRequest req
+            , TraceWriter log
+            )
+        {
+            log.Info("C# HTTP trigger function processed a request.");
+
+            throw new NotImplementedException();
+        }
+
+        [FunctionName(nameof(GetTestById))]
+        public static IActionResult GetTestById(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "test/{id}")]
+            HttpRequest req
+            , TraceWriter log
+            , string id
+            )
+        {
+            log.Info("C# HTTP trigger function processed a request.");
+
+            throw new NotImplementedException();
+        }
+
+        [FunctionName(nameof(ProcessTest))]
+        public static IActionResult ProcessTest(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "test/{id}")]
+            HttpRequest req
+            , TraceWriter log
+            , string id
+            )
+        {
+            log.Info("C# HTTP trigger function processed a request.");
+
+            throw new NotImplementedException();
+        }
+
+        [FunctionName(nameof(SetTestStatus))]
+        public static IActionResult SetTestStatus(
+            [HttpTrigger(AuthorizationLevel.Function, "put", Route = "test/{id}/status")]
+            HttpRequest req
+            , TraceWriter log
+            , string id
+            )
+        {
+            log.Info("C# HTTP trigger function processed a request.");
+
+            throw new NotImplementedException();
+        }
+
+        [FunctionName(nameof(TestSummery))]
+        public static IActionResult TestSummery(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "test/{id}/summery")]
+            HttpRequest req
+            , TraceWriter log
+            , string id
+            )
+        {
+            log.Info("C# HTTP trigger function processed a request.");
+
+            throw new NotImplementedException();
+        }
+    }
+}
