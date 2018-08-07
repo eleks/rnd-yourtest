bca3fbf10a866d832d1726f5f4889a2df39d9e19
Roman Rudyak <roma.rudyak@gmail.com>
2018-08-07 16:27:29 +0300

    add data population provider

---
+++ src/Backend/YourTest.REST/YourTest.REST/Data/DemoDataProvider.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using YourTest.REST.Models;
+using YourTest.REST.Repository;
+using System.Collections.Generic;
+
+namespace YourTest.REST.Data
+{
+    public class DemoDataProvider : IDataProvider<Test>
+    {
+        public void Seed(IRepository<Test> repository)
+        {
+            List<Test> demoData = new List<Test>();
+            demoData.Add(CreateEasyTest());
+
+            foreach (var d in demoData)
+            {
+                repository.Insert(d);
+            }
+        }
+
+        private Test CreateEasyTest()
+        {
+            return new Test
+            {
+                Id = 1,
+                Name = "Easy Test",
+                Questions = new List<Question>
+                    {
+                        new Question
+                        {
+                            Id = 1,
+                            Description = "What sky color is?",
+                            Answare = "blue",
+                            PossibleAnswares = new []
+                            {
+                                "grean",
+                                "red",
+                                "blue",
+                                "pink"
+                            }
+                        },
+                        new Question
+                        {
+                            Id = 2,
+                            Description = "2 + 2 = ?",
+                            Answare = "4",
+                            PossibleAnswares = new []
+                            {
+                                "3",
+                                "8",
+                                "6",
+                                "4"
+                            }
+                        },
+                    }
+            };
+        }
+    }
+}
---
+++ src/Backend/YourTest.REST/YourTest.REST/Data/IDataProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using YourTest.REST.Repository;
+using YourTest.REST.Models;
+namespace YourTest.REST.Data
+{
+    public interface IDataProvider<TModel>
+        where TModel : ModelBase
+    {
+        void Seed(IRepository<TModel> repository);
+    }
+}
