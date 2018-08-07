c3c4a29d51d97e8b94ca8f9ac81918b422f47857
Roman Rudyak <roma.rudyak@gmail.com>
2018-08-07 12:57:46 +0300

    Add Repository API declaration

---
+++ src/Backend/YourTest.REST/YourTest.REST/Repository/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using YourTest.REST.Models;
+using System.Linq;
+namespace YourTest.REST.Repository
+{
+    public interface IRepository<TModel>
+        where TModel : ModelBase
+    {
+        IQueryable<TModel> Query();
+    }
+}
