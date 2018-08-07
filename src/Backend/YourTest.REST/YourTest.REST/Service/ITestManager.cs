784179a375468ba1d4a04ae9aa0ea1877e81baf8
Roman Rudyak <roma.rudyak@gmail.com>
2018-08-07 12:58:02 +0300

    Add TestManager api declaration

---
+++ src/Backend/YourTest.REST/YourTest.REST/Service/ITestManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using YourTest.REST.Models;
+namespace YourTest.REST.Service
+{
+    public interface ITestManager
+    {
+        Task<IEnumerable<Test>> GetAll();
+        Task<Test> GetById(Int32 id);
+    }
+}
---
+++ src/Backend/YourTest.REST/YourTest.REST/Service/TestManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using YourTest.REST.Models;
+using YourTest.REST.Repository;
+using System.Linq;
+
+namespace YourTest.REST.Service
+{
+    public class TestManager : ITestManager
+    {
+        public TestManager(IRepository<Test> repository) => _repo = repository;
+
+        public async Task<IEnumerable<Test>> GetAll() => await Task.Run<IEnumerable<Test>>(() => _repo.Query().ToArray());
+
+        public async Task<Test> GetById(int id) => await Task.Run(() => _repo.Query().FirstOrDefault(t => t.Id == id));
+
+        private readonly IRepository<Test> _repo;
+    }
+}
