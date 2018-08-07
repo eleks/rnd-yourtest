c9a4d99e5d0d0a3aab23b3fd31b6efda148c4c61
Roman Rudyak <roma.rudyak@gmail.com>
2018-08-07 16:26:33 +0300

    Implemented in memory repository

---
+++ src/Backend/YourTest.REST/YourTest.REST/Repository/InMemoryRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using YourTest.REST.Models;
+using System.Collections.Generic;
+
+namespace YourTest.REST.Repository
+{
+    public class InMemoryRepository<TModel> : IRepository<TModel>
+        where TModel : ModelBase
+    {
+        public InMemoryRepository()
+        {
+            _data = new Dictionary<Int32, TModel>();
+        }
+
+        public IQueryable<TModel> Query() => _data.Values.AsQueryable();
+
+        public void Insert(TModel model) => _data[model.Id] = model;
+
+        private readonly Dictionary<Int32, TModel> _data;
+    }
+}
