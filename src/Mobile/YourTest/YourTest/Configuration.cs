54317cbe4d01f7d935657f86557b33329d84eed5
Roman Rudyak <roma.rudyak@gmail.com>
2018-08-10 15:42:21 +0300

    Add configuration file to Mobile Core project

---
+++ src/Mobile/YourTest/YourTest/Configuration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+// NOTE: Do not remove. Used by generated code.
+using YourTest.Helpers;
+
+namespace YourTest
+{
+    // note: folow secrets initialization from https://github.com/dansiegel/Mobile.BuildTools/wiki/Build-Secrets
+    // In VS4Mac Secrets can be treated as error but it all builds
+    public static class Configuration
+    {
+        public static String AADClientId => Secrets.AzureClientId;
+        public static String AADRedirectUri => Secrets.AzureRedirectUri;
+        public static String AADAuthority { get; } = "https://login.windows.net/common";
+        public static String AADResource { get; } = "https://graph.microsoft.com";
+
+    }
+}
