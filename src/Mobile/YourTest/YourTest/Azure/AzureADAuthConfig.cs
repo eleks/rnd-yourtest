4da163946497c60419e1a69e4384535c269a81e6
Roman Rudyak <roma.rudyak@gmail.com>
2018-08-10 12:35:54 +0300

    Add Azure Auth Contract to Core Project

---
+++ src/Mobile/YourTest/YourTest/Azure/AzureADAuthConfig.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace YourTest.Azure
+{
+    public sealed class AzureADAuthConfig
+    {
+        public string Authority { get; set; }
+        public string Resource { get; set; }
+        public string ClientId { get; set; }
+        public string ReturnUri { get; set; }
+    }
+}
---
+++ src/Mobile/YourTest/YourTest/Azure/IAuthenticator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+namespace YourTest.Azure
+{
+    public interface IAuthenticator
+    {
+        Task<AuthenticationResult> Authenticate(AzureADAuthConfig config);
+    }
+}
