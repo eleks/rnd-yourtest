380cbb87db559299f46066c1c124c3529fb19185
Roman Rudyak <roma.rudyak@gmail.com>
2018-08-14 15:57:16 +0300

    set correct authority for azure auth

--- src/Mobile/YourTest/YourTest/Configuration.cs
+++ src/Mobile/YourTest/YourTest/Configuration.cs
@@ -10,7 +10,7 @@ namespace YourTest
     {
         public static String AADClientId => Secrets.AzureClientId;
         public static String AADRedirectUri => Secrets.AzureRedirectUri;
-        public static String AADAuthority { get; } = "https://login.windows.net/common";
+        public static String AADAuthority { get; } = "https://login.microsoftonline.com/common";
         public static String AADResource => Secrets.AzureResource;
     }
 }
