74db30c83b78e622091496f560fc0dcefc384fc3
Roman Rudyak <roma.rudyak@gmail.com>
2018-08-15 16:58:34 +0300

    add IsBusy to Base ViewModel

--- src/Mobile/YourTest/YourTest/ViewModels/ViewModelBase.cs
+++ src/Mobile/YourTest/YourTest/ViewModels/ViewModelBase.cs
@@ -6,6 +6,12 @@ namespace YourTest.ViewModels
     public abstract class ViewModelBase : BindableBase
     {
 
+        private bool _isBusy;
+        public Boolean IsBusy
+        {
+            get => _isBusy;
+            set => SetProperty(ref _isBusy, value);
+        }
     }
 }
 
