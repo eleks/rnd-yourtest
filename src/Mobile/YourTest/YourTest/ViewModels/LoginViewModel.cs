b209433512d866039a228d42f36ddafa1ba8ae98
Roman Rudyak <roma.rudyak@gmail.com>
2018-08-15 16:58:57 +0300

    Add LoginCiewModel with login process

---
+++ src/Mobile/YourTest/YourTest/ViewModels/LoginViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Input;
+using YourTest.Azure;
+using Prism.Commands;
+using System.Threading.Tasks;
+using Prism.Navigation;
+using System.Diagnostics;
+
+namespace YourTest.ViewModels
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        public ICommand LoginCommand { get; }
+
+        public LoginViewModel(
+            IAuthenticator authenticator,
+            INavigationService navigationService
+            )
+        {
+            _authenticator = authenticator;
+            _navigationService = navigationService;
+            LoginCommand = new DelegateCommand(async () => await LoginAsync());
+        }
+
+        private async Task LoginAsync()
+        {
+            IsBusy = true;
+            try
+            {
+                var res = await _authenticator.AuthenticateAsync();
+                // todo await _navigationService.NavigateAsync(nameof(MainViewModel));
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        private readonly IAuthenticator _authenticator;
+        private readonly INavigationService _navigationService;
+    }
+}
+
