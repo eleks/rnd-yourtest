6d22a25b342ef0d0dfbc327f38d1a3b55e0893eb
Roman Rudyak <roma.rudyak@gmail.com>
2018-08-16 14:06:10 +0300

    Registered http stack in IoC

--- src/Mobile/YourTest/YourTest/App.xaml.cs
+++ src/Mobile/YourTest/YourTest/App.xaml.cs
@@ -7,6 +7,9 @@ using Xamarin.Forms.Xaml;
 
 using YourTest.Views;
 using YourTest.ViewModels;
+using System.Net.Http;
+using YourTest.Http;
+using YourTest.Auth;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace YourTest
@@ -31,6 +34,36 @@ namespace YourTest
             RegisterViews(containerRegistry);
             var builder = containerRegistry.GetBuilder();
             RegisterConfigs(builder);
+            RegisterHttpHandlerStack(builder);
+        }
+
+        private void RegisterHttpHandlerStack(ContainerBuilder containerBuilder)
+        {
+            containerBuilder
+                .Register(c => new MemoryTokenStore())
+                .As<ITokenStore>()
+                .SingleInstance();
+
+            containerBuilder.Register(c =>
+            {
+                HttpMessageHandler handler = c.ResolveNamed<HttpMessageHandler>(IoCNameNativeHttpHandler);
+
+#if DEBUG
+                // For debug purposes only
+                // Must be in the bootom of real request
+                handler = new LoggerHttpMessgeHandler(handler);
+#endif
+                // Provides request with valid token
+                handler = new TokenAwareMeassageHandler(handler, c.Resolve<ITokenStore>());
+
+                return handler;
+            })
+            .AsSelf();
+
+            containerBuilder
+                .Register(c => new HttpClient(c.Resolve<HttpMessageHandler>()))
+                .AsSelf()
+                .SingleInstance();
         }
 
         private static void RegisterConfigs(ContainerBuilder containerBuilder)
