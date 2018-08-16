43aa3a3ab8f8196bd4031655e0e489761bc9b209
Roman Rudyak <roma.rudyak@gmail.com>
2018-08-16 13:43:09 +0300

    Add token aware http message handler

---
+++ src/Mobile/YourTest/YourTest/Http/TokenAwareMeassageHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using YourTest.Auth;
+
+using Header = System.Net.Http.Headers.AuthenticationHeaderValue;
+
+namespace YourTest.Http
+{
+    public class TokenAwareMeassageHandler : DelegatingHandler
+    {
+        private readonly ITokenStore _tokenStore;
+
+        public TokenAwareMeassageHandler(
+            HttpMessageHandler innerHandler,
+            ITokenStore tokenStore)
+            : base(innerHandler)
+        {
+            _tokenStore = tokenStore;
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if (_tokenStore.HasToken)
+            {
+                request.Headers.Authorization = new Header("Bearer", _tokenStore.Token);
+            }
+
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
