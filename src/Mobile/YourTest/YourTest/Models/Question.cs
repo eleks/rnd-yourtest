0ec0085fa9b275bf40cb08c6e1a38a9c1df75547
Roman Rudyak <roma.rudyak@gmail.com>
2018-08-27 14:06:49 +0300

    Add models for REST api

---
+++ src/Mobile/YourTest/YourTest/Models/Question.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace YourTest.Models
+{
+    public class Question : ModelBase
+    {
+        public String Description { get; set; }
+        public IList<String> PossibleAnswers { get; set; }
+        public QuestionType Type { get; set; }
+
+        public Question()
+        {
+            PossibleAnswers = new List<String>();
+        }
+    }
+}
