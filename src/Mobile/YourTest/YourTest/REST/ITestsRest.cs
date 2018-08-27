456160b6667c3f03b6ea2df382ab69e3bfbabbf0
Roman Rudyak <roma.rudyak@gmail.com>
2018-08-27 14:07:10 +0300

    Add Tests REST service

---
+++ src/Mobile/YourTest/YourTest/REST/ITestsRest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using Refit;
+using YourTest.Models;
+namespace YourTest.REST
+{
+    public interface ITestsRest
+    {
+        [Get("/api/test")]
+        Task<Test[]> GetAllAsync();
+        [Get("/api/test/{id}")]
+        Task<Test> GetBbyIdAsync(Int32 id);
+        [Get("/api/test/{id}")]
+        Task<TestSummery> ProcessTestAsync(Int32 id, [Body]QuestionAnswer[] answers);
+    }
+}
