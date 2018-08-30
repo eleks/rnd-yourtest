5a2335be3c00575dbd29ac5d8294c7657cbbb0bc
Roman Rudyak <roma.rudyak@gmail.com>
2018-08-30 16:30:13 +0300

    Add splash screen to Android project

---
+++ src/Mobile/YourTest/YourTest.Android/SplashScreenActivity.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace YourTest.Droid
+{
+    [Activity(
+        MainLauncher = true
+        , NoHistory = true
+        , Theme = "@style/MainTheme.Splash"
+        )]
+    public class SplashScreenActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
