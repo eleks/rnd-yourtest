0b17ed99943f15f2b4c8a6b57263d9f5deb7a2b2
Roman Rudyak <roma.rudyak@gmail.com>
2018-09-04 14:58:21 +0300

    add file with constants for file names

---
+++ src/Backend/YourTest.REST/YourTest.REST/Constants.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace YourTest.REST
+{
+    internal static class Constants
+    {
+        public static String FileDir { get; } = "Store";
+        public static String DataFile { get; } = "tests.json";
+    }
+}
