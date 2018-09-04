a95f59ef4ae9623730d13d5f2a9c677580f8bbe8
Roman Rudyak <roma.rudyak@gmail.com>
2018-09-04 14:45:53 +0300

    Add file data provider

---
+++ src/Backend/YourTest.REST/YourTest.REST/Data/FileDataProvider.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using YourTest.REST.Models;
+using YourTest.REST.Repository;
+
+namespace YourTest.REST.Data
+{
+    public class FileDataProvider : IDataProvider<Test>
+    {
+        private readonly string _rootDir;
+        private readonly string _filePath;
+
+        public FileDataProvider(String rootDir)
+        {
+            _rootDir = rootDir;
+            _filePath = $"{_rootDir}\tests.json";
+        }
+
+        public void Seed(IRepository<Test> repository)
+        {
+            var tests = JsonConvert.DeserializeObject<Test[]>(File.ReadAllText(_filePath));
+
+            foreach (var t in tests)
+            {
+                repository.Insert(t);
+            }
+        }
+
+        private static string GetScriptPath() => Path.Combine(GetEnvironmentVariable("HOME"), @"site\wwwroot");
+
+        private static string GetEnvironmentVariable(string name) => System.Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Process);
+    }
+}
