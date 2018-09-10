938c0af49325a6b83686d57e1012f1da41c129c0
Roman Rudyak <roma.rudyak@gmail.com>
2018-09-10 16:37:25 +0300

    Add BooleanToValueConverter

---
+++ src/Mobile/YourTest/YourTest/Converters/BooleanToValueConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+namespace YourTest.Converters
+{
+    public class BooleanToValueConverter<TValue> : IValueConverter
+    {
+        public TValue TrueValue { get; set; }
+        public TValue FalseValue { get; set; }
+
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((bool)value) ? TrueValue : FalseValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
