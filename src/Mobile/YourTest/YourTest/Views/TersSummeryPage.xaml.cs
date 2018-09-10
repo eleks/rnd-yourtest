d861c94b028f632c2e9dec1e4275b25d09eba675
Roman Rudyak <roma.rudyak@gmail.com>
2018-09-10 10:22:22 +0300

    Add empty TestSummeryPage

---
+++ src/Mobile/YourTest/YourTest/Views/TersSummeryPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace YourTest.Views
+{
+    public partial class TestSummeryPage : ContentPage
+    {
+        public TestSummeryPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
