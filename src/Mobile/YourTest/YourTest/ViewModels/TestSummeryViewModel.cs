9291c1f827191e2b7d3f4155456a80505f9a9e43
Roman Rudyak <roma.rudyak@gmail.com>
2018-09-10 10:22:40 +0300

    Add empty TestSummeryViewModel

---
+++ src/Mobile/YourTest/YourTest/ViewModels/TestSummeryViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace YourTest.ViewModels
+{
+    public class TestSummeryViewModel : ViewModelBase
+    {
+        public TestSummeryViewModel()
+        {
+        }
+    }
+}
