058d21b82ec6f20e359b01874c545db4a190e842
Roman Rudyak <roma.rudyak@gmail.com>
2018-09-11 16:53:38 +0300

    add Awareness OnNavigating to viewmodel

--- src/Mobile/YourTest/YourTest/ViewModels/ViewModelBase.cs
+++ src/Mobile/YourTest/YourTest/ViewModels/ViewModelBase.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using Prism.Mvvm;
 using Prism.AppModel;
+using Prism.Navigation;
 
 namespace YourTest.ViewModels
 {
-    public abstract class ViewModelBase : BindableBase, IPageLifecycleAware
+    public abstract class ViewModelBase : BindableBase, IPageLifecycleAware, INavigatingAware
     {
 
         private bool _isBusy;
@@ -17,8 +18,13 @@ namespace YourTest.ViewModels
         void IPageLifecycleAware.OnAppearing() => OnAppearing();
         void IPageLifecycleAware.OnDisappearing() => OnDisappearing();
 
+        void INavigatingAware.OnNavigatingTo(NavigationParameters parameters) => OnNavigatingTo(parameters);
+
         protected virtual void OnAppearing() { }
         protected virtual void OnDisappearing() { }
+
+        protected virtual void OnNavigatingTo(NavigationParameters parameters) { }
+
     }
 }
 
