1201d4aa1587d9494ccaf41cdb5f5b9fe4343b0e
Vitalii Smal <vitalii.smal@gmail.com>
2018-09-12 12:28:54 +0300

    Implement JetGesture for objects manipulation.

---
+++ src/MixedReality/YourTest.HoloLens/Assets/Scripts/JetGesture.cs
@@ -0,0 +1,28 @@
+﻿using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+public class JetGesture : MonoBehaviour, IManipulationHandler
+{
+    void IManipulationHandler.OnManipulationStarted(ManipulationEventData eventData)
+    {
+        InputManager.Instance.PushModalInputHandler(gameObject);
+        manipulationOriginalPosition = transform.position;
+    }
+
+    void IManipulationHandler.OnManipulationUpdated(ManipulationEventData eventData)
+    {
+        transform.position = manipulationOriginalPosition + eventData.CumulativeDelta;
+    }
+
+    void IManipulationHandler.OnManipulationCompleted(ManipulationEventData eventData)
+    {
+        InputManager.Instance.PopModalInputHandler();
+    }
+
+    void IManipulationHandler.OnManipulationCanceled(ManipulationEventData eventData)
+    {
+        InputManager.Instance.PopModalInputHandler();
+    }
+
+    private Vector3 manipulationOriginalPosition = Vector3.zero;
+}
