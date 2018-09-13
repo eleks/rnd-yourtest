f4dc0a12bb3b073a2f1143e77c608bcf96dcde97
Roman Rudyak <roma.rudyak@gmail.com>
2018-09-13 13:22:10 +0300

    add Etalon value converter

---
+++ src/Mobile/YourTest/YourTest/Converters/EtalonValueToBoleanValueConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+using System.Collections.Generic;
+namespace YourTest.Converters
+{
+    public class EtalonValueToBoleanValueConverter<TEatalon, TValue> : IValueConverter
+    {
+        public TEatalon EtalonValue { get; set; }
+
+        public TValue TrueValue { get; set; }
+        public TValue FalseValue { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            TEatalon v = (TEatalon)value;
+
+            return EqualityComparer<TEatalon>.Default.Equals(EtalonValue, v)
+                ? TrueValue
+                : FalseValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
