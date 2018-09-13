3a2aa34871af67e38ea34d50d555f43344059146
Roman Rudyak <roma.rudyak@gmail.com>
2018-09-13 13:47:03 +0300

    add received icon for notify user about answer in Mixed Reality

--- src/Mobile/YourTest/YourTest.Android/Resources/Resource.designer.cs
+++ src/Mobile/YourTest/YourTest.Android/Resources/Resource.designer.cs
@@ -6136,26 +6136,26 @@ namespace YourTest.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
-			// aapt resource value: 0x7f02013a
-			public const int avd_hide_password_1 = 2130837818;
-			
 			// aapt resource value: 0x7f02013b
-			public const int avd_hide_password_2 = 2130837819;
+			public const int avd_hide_password_1 = 2130837819;
 			
 			// aapt resource value: 0x7f02013c
-			public const int avd_hide_password_3 = 2130837820;
+			public const int avd_hide_password_2 = 2130837820;
+			
+			// aapt resource value: 0x7f02013d
+			public const int avd_hide_password_3 = 2130837821;
 			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
 			
-			// aapt resource value: 0x7f02013d
-			public const int avd_show_password_1 = 2130837821;
-			
 			// aapt resource value: 0x7f02013e
-			public const int avd_show_password_2 = 2130837822;
+			public const int avd_show_password_1 = 2130837822;
 			
 			// aapt resource value: 0x7f02013f
-			public const int avd_show_password_3 = 2130837823;
+			public const int avd_show_password_2 = 2130837823;
+			
+			// aapt resource value: 0x7f020140
+			public const int avd_show_password_3 = 2130837824;
 			
 			// aapt resource value: 0x7f020055
 			public const int design_bottom_navigation_item_background = 2130837589;
@@ -6808,11 +6808,11 @@ namespace YourTest.Droid
 			// aapt resource value: 0x7f02012d
 			public const int notification_icon_background = 2130837805;
 			
-			// aapt resource value: 0x7f020138
-			public const int notification_template_icon_bg = 2130837816;
-			
 			// aapt resource value: 0x7f020139
-			public const int notification_template_icon_low_bg = 2130837817;
+			public const int notification_template_icon_bg = 2130837817;
+			
+			// aapt resource value: 0x7f02013a
+			public const int notification_template_icon_low_bg = 2130837818;
 			
 			// aapt resource value: 0x7f02012e
 			public const int notification_tile_bg = 2130837806;
@@ -6824,25 +6824,28 @@ namespace YourTest.Droid
 			public const int Prev = 2130837808;
 			
 			// aapt resource value: 0x7f020131
-			public const int sad_face = 2130837809;
+			public const int received = 2130837809;
 			
 			// aapt resource value: 0x7f020132
-			public const int splashscreen = 2130837810;
+			public const int sad_face = 2130837810;
 			
 			// aapt resource value: 0x7f020133
-			public const int test = 2130837811;
+			public const int splashscreen = 2130837811;
 			
 			// aapt resource value: 0x7f020134
-			public const int test_check = 2130837812;
+			public const int test = 2130837812;
 			
 			// aapt resource value: 0x7f020135
-			public const int tooltip_frame_dark = 2130837813;
+			public const int test_check = 2130837813;
 			
 			// aapt resource value: 0x7f020136
-			public const int tooltip_frame_light = 2130837814;
+			public const int tooltip_frame_dark = 2130837814;
 			
 			// aapt resource value: 0x7f020137
-			public const int Up = 2130837815;
+			public const int tooltip_frame_light = 2130837815;
+			
+			// aapt resource value: 0x7f020138
+			public const int Up = 2130837816;
 			
 			static Drawable()
 			{
