24f1baa7f683864b18266d9870e8b854c3749b79
Roman Rudyak <roma.rudyak@gmail.com>
2018-09-14 14:27:37 +0300

    removed incorrect file

--- src/Backend/environment/create.sh
+++
@@ -1,8 +0,0 @@
-# /usr/bash
-
-az group create --name ExampleGroup --location "Central US"
-az group deployment create \
-  --name ExampleDeployment \
-  --resource-group ExampleGroup \
-  --template-file storage.json \
-  --parameters storageAccountType=Standard_GRS
