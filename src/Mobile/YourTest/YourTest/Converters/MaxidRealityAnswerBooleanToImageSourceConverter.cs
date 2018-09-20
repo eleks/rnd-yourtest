1261deb66cde21a58413119c663a8c481f96bbd1
Yaroslav Tsapiv <slaviktsapiv@gmail.com>
2018-09-20 13:35:28 +0300

    Refactor MixedRealityQuestionPageView

---
+++ src/Mobile/YourTest/YourTest/Converters/MaxidRealityAnswerBooleanToImageSourceConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace YourTest.Converters
+{
+    public class MaxidRealityAnswerBooleanToImageSourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (Boolean)value ? ImageSource.FromFile("true") : ImageSource.FromFile("false");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
