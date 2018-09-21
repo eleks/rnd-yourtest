ac910d69c952a83addbb4c7b1b0ae8dbd80fb09b
Yaroslav Tsapiv <slaviktsapiv@gmail.com>
2018-09-21 16:07:39 +0300

    Rename true false images

--- src/Mobile/YourTest/YourTest/Converters/MaxidRealityAnswerBooleanToImageSourceConverter.cs
+++ src/Mobile/YourTest/YourTest/Converters/MaxidRealityAnswerBooleanToImageSourceConverter.cs
@@ -8,7 +8,7 @@ namespace YourTest.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return (Boolean)value ? ImageSource.FromFile("true") : ImageSource.FromFile("false");
+            return (Boolean)value ? ImageSource.FromFile("correct") : ImageSource.FromFile("incorrect");
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
